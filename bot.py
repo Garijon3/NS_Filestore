c541c73493be9bb93258c0ca21c8bbc6c35255cd
Garijon3 <105334993+Garijon3@users.noreply.github.com>
2023-02-24 13:32:14 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 from pyrogram import Client
 API_ID = int(os.environ.get("API_ID", "26364421"))
 API_HASH = os.environ.get("API_HASH", "72c7598f883fa1b077358d6c86071654")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "5167283788:AAHJ84X5J1Z_Y85fj68JGFBX45uTWy9U6iE")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6047825180:AAHxFJdl2C7MdBr98Q-f-vCH1dHUTlwvnak")
 
 
 def main():
